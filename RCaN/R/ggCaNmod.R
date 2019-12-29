b27854857eda6e3327380dbc4af6d11037b13d27
hilaire drouineau <hilaire drouineau@irstea.fr>
2019-12-29 11:35:26 +0100

    working package

---
+++ RCaN/R/ggCaNmod.R
@@ -0,0 +1,74 @@
+#' ggCaNmod
+#' produces a ggraph representing the flow foodweb
+#' @param myCaNmod a CaNmod oject
+#' @param ... Additional arguments
+#' @return a ggraph
+#'
+#' @examples
+#' myCaNmod <- build_CaNmod(system.file("extdata", "CaN_template_mini.xlsx", package = "RCaN"))
+#' ggCaNmod(myCaNmod)
+#' @importFrom igraph graph_from_data_frame
+#' @importFrom ggraph ggraph
+#' @importFrom ggraph geom_edge_arc
+#' @importFrom ggraph geom_node_text
+#' @importFrom ggraph geom_edge_arc2
+#' @importFrom ggplot2 arrow
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 unit
+#' @importFrom ggraph geom_edge_loop
+#' @importFrom ggraph geom_node_point
+#' @importFrom cpgsR getBoundParam
+#' @importFrom graphics plot
+#' @export
+ggCaNmod <- function(myCaNmod) {
+  # need to order the links table so that it matches the order of the components
+  edge_graph <- myCaNmod$fluxes_def[, c("From", "To", "Trophic", "Flux")]
+  edge_graph$Trophic <- as.factor(edge_graph$Trophic)
+  names(edge_graph)[c(1:2)] <- c("from", "to")
+  edge_graph = rbind.data.frame(d = edge_graph, list("HerbZooplankton", "HerbZooplankton", 1, "F01"))
+  vertices_graph <-
+    myCaNmod$components_param[, c("Component", "in_out")]
+
+  g <- graph_from_data_frame(d = edge_graph, vertices = vertices_graph)
+
+  gg_foodweb <- ggraph(g, 'circle') +
+    geom_node_point(aes(
+      x = x * 1.05,
+      y = y * 1.05,
+      colour = in_out
+    ),
+    size = 5,
+    alpha = 0.33) +
+    geom_node_text(aes(
+      x = x * 1.1,
+      y = y * 1.1,
+      label = name
+    ),
+    size = 3,
+    alpha = 1) +
+    geom_edge_arc2(
+      aes(label = Flux, colour = Trophic),
+      strength = 0.1,
+      alpha = 0.33,
+      width = 1,
+      arrow = arrow(
+        angle = 30,
+        length = unit(0.1, "inches"),
+        ends = "last",
+        type = "closed"
+      )
+    ) +
+    geom_edge_loop(
+      aes(
+        span = 90,
+        direction = 90,
+        label = Flux,
+        colour = Trophic
+      ),
+      width = 1,
+      alpha = 0.33
+    ) +
+    theme(aspect.ratio = 1)
+
+}
---
+++ RCaN/R/pdfCaNmod.R
@@ -0,0 +1,70 @@
+#' pdfCaNmod
+#' creates a pdf summary of the case study
+#' @param myCaNmod a CaNmod oject
+#' @param output_file name of the output file
+#' @export
+#'
+#' @examples
+#' myCaNmod <- build_CaNmod(system.file("extdata", "CaN_template_mini.xlsx", package = "RCaN"))
+#' pdfCaNmod(myCaNmod)
+#' @importFrom gridExtra ttheme_default
+#' @importFrom gridExtra grid.table
+#' @importFrom tidyr gather
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 rel
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 facet_wrap
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices pdf
+#' @importFrom graphics plot.new
+#' @importFrom stats time
+
+pdfCaNmod <- function(myCaNmod, output_file = "summaryCaNmod.pdf") {
+  # step 2: draw a table with the input parameters
+  pdf('output_file')
+  mytheme <- gridExtra::ttheme_default(
+    core = list(fg_params = list(cex = 0.6)),
+    colhead = list(fg_params = list(cex = 0.6)),
+    rowhead = list(fg_params = list(cex = 0.6))
+  )
+  grid.table(myCaNmod$components, theme = mytheme)
+
+  gg_foodweb <- ggCaNmod(myCaNmod)
+
+  print(gg_foodweb)
+
+  # step 4: draw all time series
+  ns <- dim(myCaNmod$series)[2] - 1
+  for (i in 1:ceiling(ns / 9)) {
+    TS <-
+      gather(myCaNmod$series[, ((i - 1) * 9 + 2):(min(ns, (i * 9)) + 1)], key =
+               'series', value = 'value')
+    TS$time <- rep(myCaNmod$series[, 1], min(ns, (i * 9)) - (i - 1) * 9)
+    gg_timeseries <- ggplot(TS, aes(x = time, y = value)) +
+      facet_wrap(. ~ series, scales = "free") +
+      geom_line(colour = 'darkcyan') +
+      geom_point(colour = 'darkcyan', size = rel(.8)) +
+      labs(x = 'time', y = 'value') +
+      theme(axis.text = element_text(size = rel(.5)))
+    print(gg_timeseries)
+  }
+
+  # step 5: draw a table with all the constraints
+  nc = dim(myCaNmod$constraints)[1] # number of constraints
+  mytheme <- gridExtra::ttheme_default(
+    core = list(fg_params = list(cex = 0.9)),
+    colhead = list(fg_params = list(cex = 0.9)),
+    rowhead = list(fg_params = list(cex = 0.9))
+  )
+  for (i in 1:ceiling(nc / 20)) {
+    plot.new()
+    grid.table(myCaNmod$constraints[((i - 1) * 20 + 1):min(nc, (i * 20)), ], theme = mytheme)
+  }
+
+  dev.off()
+}
