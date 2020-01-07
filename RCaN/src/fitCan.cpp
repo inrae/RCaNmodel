fe0fb2ba8ae8eca91a6b68ae38991f53eb5292b2
hilaire drouineau <hilaire drouineau@irstea.fr>
2020-01-07 11:03:01 +0100

    removed unused variable cpp

--- RCaN/src/fitCan.cpp
+++ RCaN/src/fitCan.cpp
@@ -17,15 +17,13 @@ using Eigen::VectorXd;
 // [[Rcpp::export]]
 List fitCaN(const int N,const Eigen::MatrixXd &A ,const Eigen::VectorXd &b,const Eigen::MatrixXd &C ,const Eigen::VectorXd &v,const Eigen::MatrixXd &L, const Eigen::VectorXd M,const Eigen::VectorXd &x0) {
   int p=A.cols();
-  int m=A.rows();
-  int p2=C.cols();
   int m2=C.rows();
-  MatrixXd F(N,p);
-  MatrixXd B(N,M.size());
+  MatrixXd F(N, p);
+  MatrixXd B(N, M.size());
   if(m2>0){ //there are equality constraints
-    F=cpgsR::cpgsEquality(N,A,b,C,v,x0);
+    F=cpgsR::cpgsEquality(N, A, b, C, v, x0);
   } else{
-    F=cpgsR::cpgs(N,A,b,x0);
+    F=cpgsR::cpgs(N, A, b, x0);
   }
   for (int i=0;i<N;++i){
     B.row(i)=L*F.row(i).transpose()+M;
