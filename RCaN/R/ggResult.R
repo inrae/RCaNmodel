cfc490cf329ae28b8d891af877cc49e903839e60
hilaire drouineau <hilaire drouineau@irstea.fr>
2020-02-08 22:43:57 +0100

    correction d'un bug dans le grep et intégration de la fonction de Benjamin

--- RCaN/R/ggResult.R
+++ RCaN/R/ggResult.R
@@ -6,6 +6,8 @@
 #' @param myCaNmod a CaNmod object
 #' @param param the name (or a vector of name) of a parameter (either a flow or
 #' a biomass)
+#' @param ylab default Biomass Flux
+#' @param plot_series if yes, draw three example of trajectories
 #' @return a ggplot
 #' @details the line corresponds to median of the mcmc simulation, the ribbon
 #' corresponds to quantiles 2.5% and 97.5%
@@ -29,29 +31,59 @@
 
 #' @export
 #'
-ggResult <- function(mcmc_res, myCaNmod, param) {
+ggResult <- function(mcmc_res, myCaNmod, param, plot_series, ylab="Biomass/Flux") {
   mat_res <- as.matrix(mcmc_res)
   quantiles <- do.call("rbind.data.frame", lapply(param, function(p) {
-    columns <- grep(paste(p, "\\[", sep = ""), colnames(mat_res))
+    columns <- which(startsWith(colnames(mat_res), paste(p, "[", sep = "")))
     if (length(columns) == 0)
       stop("param not recognized")
     quantiles <-
       data.frame(t(apply(
-        mat_res[, columns], 2, quantile, probs = c(.025, .5, .975)
+        mat_res[, columns],
+        2,
+        quantile,
+        probs = c(0, .025, 0.25, .50, .75, .975, 1)
       )),
       year = myCaNmod$series$Year,
       series = as.character(p))
   }))
-  names(quantiles)[1:3] <- c("q2.5", "q50", "q97.5")
-  ggplot() + geom_line(data = quantiles, aes_string(x = "year", y = "q50", col =
-                                                      "series")) + geom_ribbon(
-                                                        data = quantiles,
-                                                        aes_string(
-                                                          x = "year",
-                                                          ymin = "q2.5",
-                                                          ymax = "q97.5",
-                                                          fill = "series"
-                                                        ),
-                                                        alpha = .33
-                                                      ) + ylab(param)
+  names(quantiles)[1:7] <- c("q0", "q2.5", "q25", "q50", "q75", "q97.5", "q100")
+  g <- ggplot() +
+    geom_ribbon(data = quantiles,
+                aes_string(x = "year",ymin = "q0",ymax = "q100",fill = "series"),
+                alpha = .33) +
+    geom_ribbon(data = quantiles,
+                aes_string(x = "year",ymin = "q2.5",ymax = "q97.5",fill = "series"),
+                alpha = .33) +
+    geom_ribbon(data = quantiles,
+                aes_string(x = "year",ymin = "q25",ymax = "q75",fill = "series"),
+                alpha = .33) +
+    ylab(ylab) +
+    facet_wrap(quantiles$series,scales='free')
+
+  if (plot_series){
+    fewseries <- do.call("rbind.data.frame", lapply(param, function(p) {
+      columns <- which(startsWith(colnames(mat_res), paste(p, "[", sep = "")))
+      if (length(columns) == 0)
+        stop("param not recognized")
+      selectedsamples <- sample(seq_len(nrow(mat_res)), size=3)
+      fewseries <-
+        data.frame(t(apply(
+          mat_res[selectedsamples, columns],2,identity
+        )),
+        year = myCaNmod$series$Year,
+        series = as.character(p))
+    }))
+    names(fewseries)[1:3] <- c("S1", "S2", "S3")
+    g <- g + geom_path(data = fewseries,
+                       aes_string(x = "year", y = "S1", col ="series"),
+                       lty = "solid") +
+      geom_path(data = fewseries,
+                aes_string(x = "year", y = "S2", col ="series"),
+                lty = "twodash") +
+      geom_path(data = fewseries,
+                aes_string(x = "year", y = "S3", col ="series"),
+                lty = "longdash")
+  }
+  return (g)
 }
