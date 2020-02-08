9fa94aabcddf596132a0168904e337e3c5c77d06
hilaire drouineau <hilaire drouineau@irstea.fr>
2020-02-08 14:56:09 +0100

    bug correction

--- RCaN/R/build_vector_constraint.R
+++ RCaN/R/build_vector_constraint.R
@@ -12,74 +12,76 @@
 #' @importFrom symengine get_args
 
 
-build_vector_constraint <- function(eq_constraint, symbolic_enviro) {
-  coeff_const <-
-    Matrix::Matrix(0, 1, length(symbolic_enviro$param), sparse = TRUE)
-  colnames(coeff_const) <- as.character(symbolic_enviro$param)
-  basic_constraint <- (eq_constraint)
+build_vector_constraint <-
+  function(eq_constraint, symbolic_enviro) {
+    coeff_const <-
+      Matrix::Matrix(0, 1, length(symbolic_enviro$param), sparse = TRUE)
+    colnames(coeff_const) <- as.character(symbolic_enviro$param)
+    basic_constraint <- (eq_constraint)
 
-  #We know check that there is no division
-  if(get_type(basic_constraint)=="Add"){
-    denom <- lapply(as.list(get_args(basic_constraint)),
-                    function(x){
-                      if (!get_type(x) %in% c("RealDouble","Symbol")){
-                        members <- as.list(get_args(x))
-                        types <- sapply(members,get_type)
-                        if ("Pow" %in% types)
-                          return(members[[which(types == "Pow")]])
-                        return(S(1))
-                      } else{
-                        return(S(1))
-                      }
-                    })
+    #We know check that there is no division
+    if (get_type(basic_constraint) == "Add") {
+      denom <- lapply(as.list(get_args(basic_constraint)),
+                      function(x) {
+                        if (!get_type(x) %in% c("RealDouble", "Symbol")) {
+                          members <- as.list(get_args(x))
+                          types <- sapply(members, get_type)
+                          if ("Pow" %in% types)
+                            return(members[[which(types == "Pow")]])
+                          return(S(1))
+                        } else{
+                          return(S(1))
+                        }
+                      })
 
-    denominator <- 1/ do.call('prod',denom)
-    nb_elem <- length(as.list(get_args(basic_constraint)))
-    numerator<- do.call(sum,
-                        lapply(seq_len(nb_elem), function(x){
-                          as.list(get_args(basic_constraint))[[x]]*denominator
-                        }))
-  } else if (get_type(basic_constraint) != "Mul"){
-    numerator <- basic_constraint
-    denominator <- S(1)
-  } else {
-    members <- as.list(get_args(basic_constraint))
-    types <- sapply(members,get_type)
-    if ("Pow" %in% types){
-      numerator <- do.call(sum, members[-which(types=="Pow")])
-      denominator <- members[which(types=="Pow")]
-    } else {
+      denominator <- 1 / do.call('prod', denom)
+      nb_elem <- length(as.list(get_args(basic_constraint)))
+      numerator <- do.call(sum,
+                           lapply(seq_len(nb_elem), function(x) {
+                             as.list(get_args(basic_constraint))[[x]] * denominator
+                           }))
+    } else if (get_type(basic_constraint) != "Mul") {
       numerator <- basic_constraint
       denominator <- S(1)
+    } else {
+      members <- as.list(get_args(basic_constraint))
+      types <- sapply(members, get_type)
+      if ("Pow" %in% types) {
+        numerator <- do.call(sum, members[-which(types == "Pow")])
+        denominator <- members[which(types == "Pow")]
+      } else {
+        numerator <- basic_constraint
+        denominator <- S(1)
 
+      }
     }
+
+    basic_constraint <- expand(numerator)
+    mycoeffs <- NULL
+
+    if (get_type(basic_constraint) != "Add") {
+      if (get_type(basic_constraint) == "Symbol") {
+        mycoeffs <- 1
+        names(mycoeffs) <- get_str(basic_constraint)
+      } else {
+        return(mycoeffs <- as.numeric(basic_constraint))
+        names(mycoeffs) <- "1"
+      }
+    } else {
+      mycoeffs <- sapply(as.list(get_args(basic_constraint)), function(e) {
+        if (get_type(e) == "RealDouble") {
+          return(c("1" = as.numeric(e)))
+        } else if (get_type(e) == "Symbol") {
+          val <- 1
+          names(val) <- get_str(e)
+          return (val)
+        } else {
+          val <- as.numeric(as.list(get_args(e))[[1]])
+          names(val) <- get_str(as.list(get_args(e))[[2]])
+          return(val)
+        }
+      })
+    }
+    coeff_const[, names(mycoeffs)] <- mycoeffs
+    coeff_const
   }
-
-  basic_constraint <- expand(numerator)
-
-
-
-
-  all_elements <- get_str(basic_constraint)
-  myelem <- strsplit(gsub(" ", "", gsub(
-    " +",
-    ",+1*",
-    gsub(" -", ",-1*", all_elements, fixed = TRUE),
-    fixed = TRUE
-  )), ",", fixed = TRUE)[[1]]
-  param_used <- sort(as.character(free_symbols(basic_constraint)))
-  if (length(myelem) > length(param_used)) {
-    #there is an intercept
-    param_used <- c("1", param_used)
-  }
-  coeff_const[, param_used] <-
-    mapply(function(expr, par){
-      mycoef <- eval(parse(text = gsub(
-        paste("\\*?", par, "$", sep = ""), "", expr
-        )))
-      ifelse(is.null(mycoef), 1, mycoef)
-      },
-      myelem,
-      ifelse(param_used != "1", param_used, ""))
-  coeff_const
-}
