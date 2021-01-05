c3b379c5876cf83e6a1c0cf7fbd51a50e48ff808
hilaire drouineau <hilaire drouineau@irstea.fr>
2021-01-05 19:09:25 +0100

    addd ggDiet

---
+++ RCaN/R/ggDiet.R
@@ -0,0 +1,82 @@
+
+#' ggDiet
+#'
+#' provide ggplot of diet
+#' @param myFitCaNmod result sent by \link{fitmyCaNmod}
+#' @param species the name (or a vector of name) of the species of interest
+#' @param barplot if TRUE a barplot (default), else a violin plot
+#' @return a ggplot
+#' @details distribution of fluxes or biomass for a specific year
+#'
+#' @examples
+#' myCaNmod <- build_CaNmod(system.file("extdata", "CaN_template_mini.xlsx",
+#'  package = "RCaN"))
+#' res <- fitmyCaNmod(myCaNmod, 100)
+#' #with one series#'
+#' #with 2 series
+#' ggDiet(res,"OmniZooplankton")
+#'
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_violin
+#' @importFrom ggplot2 geom_bar
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 ylab
+#' @importFrom dplyr left_join
+#' @importFrom dplyr rename
+#' @importFrom tidyr pivot_longer
+#' @importFrom ggplot2 facet_wrap
+#' @export
+#'
+ggDiet <- function(myFitCaNmod,
+                   species,
+                   barplot = TRUE) {
+  if (!all(species %in% myFitCaNmod$CaNmod$species))
+    stop("some species are not recognized")
+  if (!is.logical(barplot))
+    stop("barplot should be logical")
+  fluxes <- myFitCaNmod$CaNmod$fluxes_def %>%
+    filter(To %in% species & Trophic)
+  mat_res <- as.matrix(myFitCaNmod$mcmc)
+  mat_res <- mat_res[, grep(paste("^(",
+                                  paste(fluxes$Flux, collapse = "|"),
+                                  ")\\[",
+                                  sep = ""),
+                            colnames(mat_res)),
+                     drop = FALSE]
+  mat_res <- as.data.frame(mat_res) %>%
+    mutate(iter =  1:nrow(mat_res)) %>%
+    pivot_longer(cols = -iter,
+                 names_to = c("Var","Year"),
+                 names_pattern = "(.*)\\[(.*)\\]",
+                 values_to = "val") %>%
+    left_join(fluxes, by = c("Var" = "Flux"))
+  mat_res <- mat_res %>%
+    select(-Var)%>%
+    group_by(To, iter, Year) %>%
+    mutate(prop_in_diet = val/sum(val)) %>%
+    rename(prey = From, predator = To)
+  if (barplot)
+    mat_res <- mat_res %>%
+    group_by(prey, predator) %>%
+    summarize(prop_in_diet = mean(prop_in_diet))
+  mat_res$predator <- factor(as.character(mat_res$predator),
+                             levels = species)
+
+
+  if (barplot){
+    g <- ggplot(data = mat_res, aes(x = predator,
+                                    y = prop_in_diet,
+                                    fill = prey)) +
+      geom_bar(position="stack", stat="identity",width=.98,color='black') +
+      theme(legend.position = "bottom")
+    } else{
+    g <- ggplot(data = mat_res, aes(x = prey,
+                                    y = prop_in_diet)) +
+      geom_violin(trim = TRUE, scale = "width", fill = "darkcyan") +
+      theme_bw() +
+      facet_wrap(vars(predator), ncol = ceiling(length(species)^0.5)) +
+      xlab('Prey') +
+      ylab('% in diet')
+  }
+  return(g)
+}
