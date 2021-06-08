0fbe834e339575396c3fe1512fd4c9a07a28567e
hilaire drouineau <hilaire drouineau@irstea.fr>
2021-06-08 21:54:02 +0200

    change order between lpsolve and clp

--- RCaN/R/getParamMinMax.R
+++ RCaN/R/getParamMinMax.R
@@ -11,31 +11,31 @@ getParamMinMax <- function(OP, p) {
   solved <- FALSE
   ntry <- 0
 
-    while (ntry < 3) {
-      if (requireNamespace("ROI.plugin.clp", quietly = TRUE)){
-        res <- ROI_solve(OP, solver = "clp", control = list(amount = 0))
-      } else {
-        res <- ROI_solve(OP,
-                         solver = "lpsolve",
-                         control = list(
-                           scaling = c("extreme",
-                                       "equilibrate",
-                                       "integers")))
-      }
-      if (res$status$code != 0 &
-          requireNamespace("ROI.plugin.clp", quietly = TRUE))
-        res <- ROI_solve(OP, solver = "glpk",
-                         control=list(tm_limit = 1000))
-
-      if (res$status$code == 0) {
-        bound <- res$solution[p]
-        solved[1] <- TRUE
-      } else  {
-        bound <- NA
-      }
-      ntry <- ntry + 1
+  while (ntry < 3) {
+    res <- ROI_solve(OP,
+                     solver = "lpsolve",
+                     control = list(
+                       scaling = c("extreme",
+                                   "equilibrate",
+                                   "integers")))
+    if (requireNamespace("ROI.plugin.clp", quietly = TRUE) &
+        res$status$code == 5){
+      res <- ROI_solve(OP, solver = "clp", control = list(amount = 0))
     }
-    res <- bound
+    if (res$status$code != 0 &
+        requireNamespace("ROI.plugin.clp", quietly = TRUE))
+      res <- ROI_solve(OP, solver = "glpk",
+                       control=list(tm_limit = 1000))
+
+    if (res$status$code == 0) {
+      bound <- res$solution[p]
+      solved[1] <- TRUE
+    } else  {
+      bound <- NA
+    }
+    ntry <- ntry + 1
+  }
+  res <- bound
 
   return (res)
 }
