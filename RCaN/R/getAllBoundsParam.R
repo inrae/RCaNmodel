9923994933089077ae23e303f30af83d86f72fa1
hilaire drouineau <hilaire drouineau@irstea.fr>
2021-06-08 14:04:57 +0200

    still not working

--- RCaN/R/getAllBoundsParam.R
+++ RCaN/R/getAllBoundsParam.R
@@ -62,7 +62,7 @@ getAllBoundsParam <- function(x,
         ip <- match(colnames(A)[p], colnames(presolved$lhs))
         ob <- rep(0, ncol(presolved$lhs))
         ob[ip] <- 1
-        ROI::objective(presolved$OP) <- L_objective(rep(1, ncol(presolved$lhs)))
+        ROI::objective(presolved$OP) <- L_objective(ob)
         set.objfn(presolved$lp_model, ob)
         lpSolveAPI::write.lp(presolved$lp_model,
                  paste0(tempdir(), "/lp_mod.lp"),
