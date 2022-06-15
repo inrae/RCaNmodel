9696634d2ec2e1855a7c387386deaf8299b4284b
hilaire drouineau <hilaire drouineau@irstea.fr>
2022-06-15 11:22:50 +0200

    fix test

--- RCaN/tests/testthat/test-builCaN.R
+++ RCaN/tests/testthat/test-builCaN.R
@@ -3,11 +3,11 @@ files <- list(system.file("extdata",
               system.file("extdata",
                           "CaN_template_mini_generic.xlsx", package = "RCaN"))
 
-
+generic <- c(FALSE, TRUE)
 
 test_that("class of outputs objects are correct", {
   for (i in seq_along(files)){
-    myCaNmod <- buildCaN(files[[i]])
+    myCaNmod <- buildCaN(files[[i]], generic = generic[i])
     expect_s4_class(myCaNmod$AAll, "Matrix")
     expect_s4_class(myCaNmod$A, "Matrix")
     expect_type(myCaNmod$v, "double")
@@ -20,9 +20,10 @@ test_that("class of outputs objects are correct", {
 
 test_that("Dimensions of outputs objects are correct", {
   for (i in seq_along(files)){
-    myCaNmod <- buildCaN(files[[i]])  expect_equal(ncol(myCaNmod$AAll),
-                                                   length(myCaNmod$species) +
-                                                     length(myCaNmod$flow) * nrow(myCaNmod$series))
+    myCaNmod <- buildCaN(files[[i]], generic = generic[i])
+    expect_equal(ncol(myCaNmod$AAll),
+                 length(myCaNmod$species) +
+                   length(myCaNmod$flow) * nrow(myCaNmod$series))
     expect_equal(ncol(myCaNmod$A),
                  length(myCaNmod$species) +
                    length(myCaNmod$flow) * nrow(myCaNmod$series))
@@ -37,14 +38,15 @@ test_that("Dimensions of outputs objects are correct", {
 
 test_that("inconsistencies in names are detected",{
   for (i in seq_along(files)){
-    myCaNmod <- buildCaN(files[[i]])
+    myCaNmod <- buildCaN(files[[i]], generic = generic[i])
     components <- myCaNmod$components_param
     components$Component[1] <- paste0(components$Component[1], " ")
 
     expect_error(buildCaN(list(components_param = components,
                                fluxes_def = myCaNmod$fluxes_def,
                                series = myCaNmod$series,
-                               constraints = myCaNmod$constraints)))
+                               constraints = myCaNmod$constraints)),
+                 generic = generic[i])
 
     fluxes <- myCaNmod$fluxes_def
     fluxes$From[1] <- paste0(components$fluxes$From[1], " ")
@@ -60,7 +62,8 @@ test_that("inconsistencies in names are detected",{
     expect_error(buildCaN(list(components_param = myCaNmod$components_param,
                                fluxes_def = fluxes,
                                series = myCaNmod$series,
-                               constraints = myCaNmod$constraints)))
+                               constraints = myCaNmod$constraints)),
+                 generic = generic[i])
   }
 
 
