ce75d3d9f7f21f8df768920b1c1b2581b6cf7b67
hilaire drouineau <hilaire drouineau@irstea.fr>
2022-08-01 15:44:03 +0200

    put sampleCaNmod tests

--- RCaN/tests/testthat/test-sampleCaN.R
+++ RCaN/tests/testthat/test-sampleCaN.R
@@ -1,7 +1,22 @@
 myCaNmod <- buildCaN(system.file("extdata", "CaN_template_mini.xlsx",
                                   package = "RCaN"))
 
-test_that("can sample", {
+test_that("sampleCaN objects with good names", {
   res <- sampleCaN(myCaNmod, 100)
   res2 <- sampleCaN(res, 100)
+  expect_equal(all(names(res) == names(res2)) &
+                all(names(res) == c("CaNmod", "mcmc", "covMat")),
+              TRUE)
+})
+
+
+test_that("sampleCaN mcmc with good dim", {
+  res <- sampleCaN(myCaNmod, 50, thin = 2, nchain = 2, ncore = 2)
+  expect_equal(coda::nchain(res$mcmc),
+               2)
+  expect_equal(coda::niter(res$mcmc),
+               50)
+  expect_equal(coda::nvar(res$mcmc),
+               nrow(myCaNmod$fluxes_def) * (nrow(myCaNmod$series) - 1) +
+                 length(myCaNmod$species) * nrow(myCaNmod$series))
 })
