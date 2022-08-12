2ae3a40419feda740addd78d2659e020f8130523
hilaire drouineau <hilaire drouineau@irstea.fr>
2022-08-12 16:13:07 +0200

    update estimate parameters with default fractional coefficient

--- RCaN/R/estimateSatiation.R
+++ RCaN/R/estimateSatiation.R
@@ -4,9 +4,8 @@
 #' @param taxon one of ("endotherm", #' "vertebrate ecotherm", "invertebrate")
 #' @param assimilation_efficiency assimilation efficiency of the species
 #' (unitless)
-#' @param digestibility average digestibility correction factor of preys
+#' @param digestibility average digestibility correction factor of prey
 #' (unitless)
-#' @param fractional Fractional properties of the ecosystem (unitless)
 #'
 #' @return satiation as explained in supplementary material in
 #' \insertCite{planque2022;textual}{RCaN}
@@ -25,8 +24,7 @@
 estimateSatiation <- function(weight,
                               taxon,
                               assimilation_efficiency = 1,
-                              digestibility = 1,
-                              fractional = 0.3){
+                              digestibility = 1){
   if (!taxon %in% c("endotherm", "vertebrate ecotherm", "invertebrate"))
     stop('taxon should be one of "endotherm", "vertebrate ecotherm",
       "inverebrate" with this method')
@@ -34,6 +32,10 @@ estimateSatiation <- function(weight,
               "endotherm" = 89.2,
               "vertebrate ecotherm" = 8.9,
               "invertebrate" = 9.7)
+  fractional <- switch(taxon,
+                       "endotherm" = 1,
+                       "vertebrate ecotherm" = 0.2,
+                       "invertebrate" = 0.3)
   sigma <- (fractional * a * (weight * 1e-3) ^ -0.25) /
     (assimilation_efficiency * digestibility)
 
