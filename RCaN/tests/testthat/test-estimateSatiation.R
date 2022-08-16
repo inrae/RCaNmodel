b8cc4a9bee9378a7a2207a8f9185a472281e6807
hilaire drouineau <hilaire drouineau@irstea.fr>
2022-08-16 15:24:06 +0200

    fix test estimate satiation

--- RCaN/tests/testthat/test-estimateSatiation.R
+++ RCaN/tests/testthat/test-estimateSatiation.R
@@ -1,5 +1,5 @@
 test_that("estimateSatiation work for copepods", {
-  expect_equal(estimateSatiation(2.35e-4, taxon = "invertebrate", 1, 0.65, 0.3),
+  expect_equal(estimateSatiation(2.35e-4, taxon = "invertebrate", 1, 0.65),
                203.3352,
                tolerance = 1e-4)
 
