febf686163caec7d9a5db743c0ae1c7966936039
hilaire drouineau <hilaire drouineau@irstea.fr>
2022-08-17 11:22:46 +0200

    fix all tests

--- RCaN/R/RCaN-package.R
+++ RCaN/R/RCaN-package.R
@@ -1,4 +1,7 @@
 ## usethis namespace: start
 #' @useDynLib RCaN, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom RcppEigen RcppEigen.package.skeleton
+#' @importFrom dqrng dqrexp
 ## usethis namespace: end
 NULL
