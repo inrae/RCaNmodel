b14a5c90f2bf4b99f2703bf2f5b9a41cc771d4ef
hilaire drouineau <hilaire drouineau@irstea.fr>
2022-09-01 14:31:28 +0200

    add an option so that a ggSeries is not facetted

--- RCaNmodel/R/ggSeries.R
+++ RCaNmodel/R/ggSeries.R
@@ -7,6 +7,8 @@
 #' a biomass)
 #' @param ylab default Biomass Flux
 #' @param plot_series if yes, draw three example of trajectories (default TRUE)
+#' @param facet if TRUE (default), each param is on a specific panel, otherwise
+#' they are combined in a single plot
 #' @return a ggplot
 #' @details the line corresponds to median of the mcmc simulation, the ribbon
 #' corresponds to quantiles 2.5% and 97.5%
@@ -34,7 +36,8 @@
 ggSeries <- function(mysampleCaNmod,
                      param,
                      plot_series = TRUE,
-                     ylab = "Biomass/Flux") {
+                     ylab = "Biomass/Flux",
+                     facet = TRUE) {
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package ggplot2 needed for this function to work. Please install it.",
          call. = FALSE)
@@ -42,10 +45,10 @@ ggSeries <- function(mysampleCaNmod,
   if (! inherits(mysampleCaNmod, "sampleCaNmod"))
     stop("you should provide a sampleCaNmod object")
   mat_res <- as.matrix(mysampleCaNmod$mcmc)
-
+  
   # take some random lines that can be drawn consistently among series
   selectedsamples <- sample(seq_len(nrow(mat_res)), size = 3)
-
+  
   quantiles <- do.call("rbind.data.frame", lapply(param, function(p) {
     columns <- which(startsWith(colnames(mat_res), paste(p, "[", sep = "")))
     if (length(columns) == 0)
@@ -64,7 +67,7 @@ ggSeries <- function(mysampleCaNmod,
       series = as.character(p))
   }))
   quantiles$series <- factor(as.character(quantiles$series),
-                                levels = param)
+                             levels = param)
   names(quantiles)[1:7] <- c("q0", "q2.5", "q25", "q50", "q75", "q97.5", "q100")
   g <- ggplot() +
     geom_ribbon(data = quantiles,
@@ -85,9 +88,11 @@ ggSeries <- function(mysampleCaNmod,
                            ymax = "q75",
                            fill = "series"),
                 alpha = .33) +
-    ylab(ylab) +
-    facet_wrap(~quantiles$series, scales = "free")
-
+    ylab(ylab) 
+  if (facet){
+    g <- g + 
+      facet_wrap(~quantiles$series, scales = "free")
+  }
   if (plot_series) {
     fewseries <- do.call("rbind.data.frame", lapply(param, function(p) {
       columns <- which(startsWith(colnames(mat_res), paste(p, "[", sep = "")))
@@ -100,11 +105,11 @@ ggSeries <- function(mysampleCaNmod,
         year = mysampleCaNmod$CaNmod$series$Year[
           paste(p, "[", mysampleCaNmod$CaNmod$series$Year, "]", sep = "") %in%
             colnames(mat_res)[columns]
-          ],
+        ],
         series = as.character(p))
     }))
     fewseries$series <- factor(as.character(fewseries$series),
-                      levels = param)
+                               levels = param)
     names(fewseries)[1:3] <- c("S1", "S2", "S3")
     g <- g + geom_path(data = fewseries,
                        aes_string(x = "year", y = "S1", col = "series"),
