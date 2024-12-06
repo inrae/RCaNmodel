6883beb23cb0d9e0b793888337adfab50760c005
hilaire drouineau <hilaire.drouineau@inrae.fr>
2024-12-06 16:46:48 +0100

    fix constraints

--- RCaNconstructorShiny/R/createEmptyConstraints.R
+++ RCaNconstructorShiny/R/createEmptyConstraints.R
@@ -5,12 +5,12 @@
 #' @export
 #'
 createEmptyConstraints <- function(){
-  tibble(id = character(0),
-             Constraint = character(0),
-             `Time-range` = character(0),
-             Active = logical(0),
-             Comment = character(0),
-             idconstraint = character(0),
-             valid = logical(0),
-             validity_comments = character(0))
+  tibble(Id = character(0),
+         Constraint = character(0),
+         `Time-range` = character(0),
+         Active = logical(0),
+         Comment = character(0),
+         idconstraint = character(0),
+         valid = logical(0),
+         validity_comments = character(0))
 }
